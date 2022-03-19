5e15302659473d6087043b691ec2069e1a7de533
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-03-19 17:58:10 +0100

    fill out the template

---
+++ src/main/java/de/mari_023/phytech/PhyTech.java
@@ -0,0 +1,11 @@
+package de.mari_023.phytech;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PhyTech implements ModInitializer {
+	public static final String MODID = "phytech";
+
+	@Override
+	public void onInitialize() {
+	}
+}
